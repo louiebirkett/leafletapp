cd8b27176f7ae34e62b6b4e85d98978200ecda5c
Burk <116655424+louiebirkett@users.noreply.github.com>
2024-11-15 15:05:24 +0000

    REST API
    added a functional backend rest API

--- server/index.js
+++ server/index.js
@@ -1,10 +1,51 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var bodyParser = require('body-parser');
+const objects = require('./objects');
 
-app.get('/', (req, res) => {
-    res.send("Hello from the server!");
-})
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.listen(8080, () => {
-    console.log("listening to port 8080");
-})
+
+// GET ALL
+app.get("/objects", async function(req, res) {
+    const allObjects = await objects.getObjects();
+    res.send(allObjects);
+    
+});
+
+// GET OBJECT
+app.get("/objects/:id", async function(req, res) {
+    const object = await objects.getObjectById(req.params.id);
+    res.send(object);
+});
+
+
+// POST OBJECT
+app.post("/objects", async function(req, res) {
+    const name = req.body.name;
+    const comment = req.body.comment;
+    await objects.addObject(name, comment);
+    res.send({"message": "Success"});
+});
+
+// PUT OBJECT
+app.put("/objects/:id", async function (req, res) {
+    const id = req.params.id;
+    const name = req.body.name;
+    const comment = req.body.comment;
+    await objects.editObject(id,name,comment);
+    res.send({"message": "Success"});
+});
+
+
+
+// DELETE OBJECT
+app.delete("/objects/:id", async function(req, res){
+    await objects.deleteObject(req.params.id);
+    res.send({"message": "Success"});
+});
+
+app.listen(process.env.PORT || 8080,function(req,res){
+    console.log("Server Started!");
+});
---
+++ server/objects.js
@@ -0,0 +1,70 @@
+const db = require('./db');
+
+// GET ALL
+function getObjects(){
+    return new Promise((resolve, reject) => {
+        db.all('SELECT * FROM skipObjects',(err, rows) => {
+            if(err)
+                reject(err);
+            else
+                resolve(rows);
+        });
+    });
+}
+
+// GET OBJECT
+function getObjectById(id){
+    return new Promise((resolve, reject) => {
+        db.all('SELECT * FROM skipObjects where id=(?)', id, (err, rows) => {
+            if(err)
+                reject(err);
+            else
+                resolve(rows);
+        })
+    })
+}
+
+// POST OBJECT
+function  addObject(name, comment){
+    return new Promise((resolve, rejects) => {
+        db.run('INSERT INTO skipObjects (name, comment) VALUES (?, ?)', name, comment, (err) => {
+            if(err)
+                reject(err);
+            else
+                resolve();
+        });
+    });
+}
+
+// PUT OBJECT
+function editObject(id, name, comment){
+    return new Promise((resolve, reject) => {
+        db.run('UPDATE skipObjects SET name = (?), comment = (?) where id = (?)', [name, comment, id], err =>{
+            if(err)
+                reject(err);
+            else    
+                resolve();
+        });
+    });
+}
+
+// DELETE OBJECT
+function deleteObject(id) {
+    return new Promise((resolve, reject) => {
+        db.run('DELETE FROM skipObjects WHERE id = (?)', id, (err) => {
+            if(err)
+                reject(err);
+            else
+                resolve();
+        });
+    });
+}
+
+
+module.exports = {
+    getObjectById,
+    getObjects,
+    addObject,
+    editObject,
+    deleteObject
+};
